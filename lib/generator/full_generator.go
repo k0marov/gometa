fbbd4b67880b1e758fa5157b8799f53f79d001f9
Sam Komarov <sam@skomarov.com>
2023-09-18 21:36:09 +0300

    implementing applying go fmt to generated files

--- lib/generator/full_generator.go
+++ lib/generator/full_generator.go
@@ -3,28 +3,18 @@ package generator
 import (
 	"github.com/k0marov/gometa/lib/generator/entity_struct"
 	"github.com/k0marov/gometa/lib/generator/repository"
+	"github.com/k0marov/gometa/lib/helpers"
 	"github.com/k0marov/gometa/lib/schema"
-	"log"
-	"os"
 	"path/filepath"
 )
 
 func Generate(outDirPath, schemaPath string) {
 	ent := schema.Parse(schemaPath)
 	crudDir := filepath.Join(outDirPath, ent.JsonName)
-	err := os.Mkdir(crudDir, 0777)
-	if err != nil {
-		log.Fatalf("failed creating package directory at %q: %v", crudDir, err)
-	}
-	entityFile, err := os.Create(filepath.Join(crudDir, "entity.go"))
-	if err != nil {
-		log.Fatalf("failed creating entity file at %q: %v", crudDir, err)
-	}
+
+	entityFile := helpers.CreateFileRecursively(filepath.Join(crudDir, "entity.go"))
 	entity_struct.Generate(ent, entityFile)
 
-	repoFile, err := os.Create(filepath.Join(crudDir, "repository.go"))
-	if err != nil {
-		log.Fatalf("failed creating repo file at %q: %v", crudDir, err)
-	}
+	repoFile := helpers.CreateFileRecursively(filepath.Join(crudDir, "repository.go"))
 	repository.Generate(ent, repoFile)
 }
--- lib/helpers/helpers.go
+++ lib/helpers/helpers.go
@@ -1,6 +1,11 @@
 package helpers
 
 import (
+	"go/format"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -10,4 +15,28 @@ func JsonNameToPascalCase(jsonName string) string {
 		camelCased += strings.ToUpper(namePart[:1]) + namePart[1:]
 	}
 	return camelCased
+}
+
+func CreateFileRecursively(path string) (file *os.File) {
+	err := os.MkdirAll(filepath.Dir(path), 0777)
+	if err != nil {
+		log.Fatalf("failed creating directory to put generated file in: %v", err)
+	}
+	file, err = os.Create(path)
+	if err != nil {
+		log.Fatalf("failed creating file for putting generated code in: %v", err)
+	}
+	return file
+}
+
+func WriteFormatted(source []byte, out io.Writer) {
+	formatted, err := format.Source(source)
+	if err != nil {
+		log.Fatalf("error when applying go fmt: %v", err)
+		return
+	}
+	_, err = out.Write(formatted)
+	if err != nil {
+		log.Fatalf("error when writing formatted code to output file: %v", err)
+	}
 }
