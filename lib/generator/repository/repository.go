5a0758ad8b204779e27d3374ad1f43bc33d00a84
Комаров Семён Андреевич <semen.komarov@ocrv.ru>
2024-05-29 10:52:41 +0300

    add context propagation everywhere

--- lib/generator/repository/repository.go
+++ lib/generator/repository/repository.go
@@ -14,6 +14,7 @@ var repoTemplate = template.Must(template.New("").Parse(`
 package {{.PackageName}} 
 
 import (
+	"context"
     "gorm.io/gorm"
 	"log"
 	"fmt"
@@ -33,26 +34,26 @@ func NewRepositoryImpl(db *gorm.DB) *RepositoryImpl {
     return &RepositoryImpl{db: db}
 }
 
-func (r *RepositoryImpl) Create(dto models.Create{{ .EntityName }}DTO) (models.{{.EntityName}}, error) {
+func (r *RepositoryImpl) Create(ctx context.Context, dto models.Create{{ .EntityName }}DTO) (models.{{.EntityName}}, error) {
 	dbModel := MapCreateDTO(dto) 
-    if err := r.db.Create(&dbModel).Error; err != nil {
+    if err := r.db.WithContext(ctx).Create(&dbModel).Error; err != nil {
 		return models.{{.EntityName}}{}, fmt.Errorf("creating entity in repo: %w", err)
 	}
 	return dbModel.ToEntity(), nil
 }
 
-func (r *RepositoryImpl) Get(id string) (models.{{ .EntityName }}, error) {
+func (r *RepositoryImpl) Get(ctx context.Context, id string) (models.{{ .EntityName }}, error) {
     dbModel := new({{ .EntityName }})
-    err := r.db.Where("id = ?", id).First(dbModel).Error
+    err := r.db.WithContext(ctx).Where("id = ?", id).First(dbModel).Error
 	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return models.{{ .EntityName }}{}, clienterrs.ErrNotFound
 	}
     return dbModel.ToEntity(), err
 }
 
-func (r *RepositoryImpl) GetAll() ([]models.{{ .EntityName }}, error) {
+func (r *RepositoryImpl) GetAll(ctx context.Context, ) ([]models.{{ .EntityName }}, error) {
 	var dbModels []*{{ .EntityName }} 
-	if err := r.db.Find(&dbModels).Error; err != nil {
+	if err := r.db.WithContext(ctx).Find(&dbModels).Error; err != nil {
 		return nil, fmt.Errorf("getting all rows from sql: %w", err) 	
 	}
 	entities := make([]models.{{ .EntityName }}, len(dbModels)) 
@@ -62,17 +63,15 @@ func (r *RepositoryImpl) GetAll() ([]models.{{ .EntityName }}, error) {
 	return entities, nil
 }
 
-func (r *RepositoryImpl) Update(entity models.{{ .EntityName }}) error {
-    return r.db.Model(&{{.EntityName}}{}).Updates(MapEntity(entity)).Error
+func (r *RepositoryImpl) Update(ctx context.Context, entity models.{{ .EntityName }}) error {
+    return r.db.WithContext(ctx).Model(&{{.EntityName}}{}).Updates(MapEntity(entity)).Error
 }
 
-func (r *RepositoryImpl) Delete(id string) error {
+func (r *RepositoryImpl) Delete(ctx context.Context, id string) error {
 	// TODO: return error if not found 
     return r.db.Delete(&{{.EntityName}}{}, id).Error
 }
 `))
-
-// TODO: добавить проброску контекста везде
 
 func Generate(out io.Writer, genCtx gen.GenerationContext) error {
 	templateData := struct {
--- lib/generator/service/service.go
+++ lib/generator/service/service.go
@@ -19,11 +19,11 @@ import (
 )
 
 type Repo interface {
-    Create(dto models.Create{{ .EntityName }}DTO) (models.{{.EntityName}}, error)
-    Get(id string) (models.{{ .EntityName }}, error) 
-    GetAll() ([]models.{{ .EntityName }}, error) 
-    Update(entity models.{{ .EntityName }}) error 
-    Delete(id string) error 
+    Create(ctx context.Context, dto models.Create{{ .EntityName }}DTO) (models.{{.EntityName}}, error)
+    Get(ctx context.Context, id string) (models.{{ .EntityName }}, error) 
+    GetAll(ctx context.Context, ) ([]models.{{ .EntityName }}, error) 
+    Update(ctx context.Context, entity models.{{ .EntityName }}) error 
+    Delete(ctx context.Context, id string) error 
 }
 
 type ServiceImpl struct {
@@ -37,12 +37,12 @@ func NewServiceImpl(repo Repo) *ServiceImpl {
 func (s *ServiceImpl) Create(ctx context.Context, dto models.Create{{ .EntityName }}DTO) (models.{{ .EntityName }}, error) {
 	logger.Debug("creating {{ .EntityName}}", "value", dto) 
     // TODO: add business logic to Service.Create
-    return s.repo.Create(dto)
+    return s.repo.Create(ctx, dto)
 }
 
 func (s *ServiceImpl) Get(ctx context.Context, id string) (models.{{ .EntityName }}, error) {
 	logger.Debug("getting {{ .EntityName}}", "id", id) 
-    entity, err := s.repo.Get(id)
+    entity, err := s.repo.Get(ctx, id)
     // TODO: add business logic to Service.Get
     return entity, err
 }
@@ -50,19 +50,19 @@ func (s *ServiceImpl) Get(ctx context.Context, id string) (models.{{ .EntityName
 func (s *ServiceImpl) GetAll(ctx context.Context) ([]models.{{ .EntityName }}, error) {
 	logger.Debug("getting all {{ .EntityName}}s") 
     // TODO: add business logic 
-    return s.repo.GetAll()
+    return s.repo.GetAll(ctx)
 }
 
 func (s *ServiceImpl) Update(ctx context.Context, entity models.{{ .EntityName }}) error {
 	logger.Debug("updating {{.EntityName}}", "value", entity) 
     // TODO: add business logic to Service.Update
-    return s.repo.Update(entity)
+    return s.repo.Update(ctx, entity)
 }
 
 func (s *ServiceImpl) Delete(ctx context.Context, id string) error {
 	logger.Debug("deleting {{.EntityName}}", "id", id) 
     // TODO: add business logic to Service.Delete
-    return s.repo.Delete(id)
+    return s.repo.Delete(ctx, id)
 }
 `))
 
@@ -82,3 +82,5 @@ func Generate(out io.Writer, genCtx gen.GenerationContext) error {
 	}
 	return nil
 }
+
+// TODO: implement pagination
